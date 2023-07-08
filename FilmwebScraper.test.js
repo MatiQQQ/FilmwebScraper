d74619cc8ace39e0d0abe5fb8351a57dac5379e4
MatiQQQ <mateusbaranski@gmail.com>
2023-07-08 16:07:42 +0200

    #4 added helpers tests in helpers.test.js

---
+++ FilmwebScraper.test.js
@@ -0,0 +1,5 @@
+const { describe, it, expect } = require("@jest/globals");
+const _ = require("lodash");
+const fs = require("fs");
+const FilmwebScraper = require("./FilmwebScraper");
+const html = require("./utils/data");
