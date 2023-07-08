53125cdb330ba821cc42bf4a3bff999f98df0c0f
MatiQQQ <mateusbaranski@gmail.com>
2023-07-08 12:34:09 +0200

    #6 deleted console logs

--- utils/helpers.js
+++ utils/helpers.js
@@ -5,9 +5,3 @@ exports.validateServiceName = (serviceName) => {
   if (String(serviceName).length === 0) isValid = false;
   return isValid;
 };
-
-console.log(this.validateServiceName());
-console.log(this.validateServiceName(null));
-console.log(this.validateServiceName(50));
-console.log(this.validateServiceName(""));
-console.log(this.validateServiceName("hbo"));
